07dc6db28c11e6a894f1f7b7f14be0537b550d39
Diego Reyes <reyesdiego@hotmail.com>
2019-09-20 09:24:45 -0300

    2 - Configurating express

--- src/startup/expressUp.js
+++ src/startup/expressUp.js
@@ -3,17 +3,31 @@ const bodyParser = require("body-parser");
 const compression = require("compression");
 const cors = require("cors");
 const http = require("http");
+const { PORT } = require("../constants");
 
 const expressUp = () => {
   const app = express();
 
+  app.disable("x-powered-by");
+  app.use(cors());
+  app.use(compression({ level: 1 }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
   app.get("/", (req, res) => {
-    res.status(200).send({ hi: "there" });
+    res.status(200).send({ hi: "Minesweeper" });
   });
 
-  app.listen(3000, (err, host) => {
-    console.log(host);
-  });
+  const server = http.createServer(app);
+  //TODO put port in constants file
+
+  server.listen(PORT, () =>
+    console.log(
+      `API running on port ${PORT}. (${process.version}) pid:${
+        process.pid
+      } - ${new Date()}`
+    )
+  );
 };
 
 module.exports = expressUp;
